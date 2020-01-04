4358ffe6569ef0ea1ddd49f3da92e66b84b5fba3
jan <jan.s@icanlocalize.com>
2020-01-04 14:53:36 +0100

    Update library setup.

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
-    readme = f.read()
+    readmeText = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    licenseText = f.read()
 
 setup(
     name='gcal-mm-status',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
+    description='I\'m learning Python here!',
+    long_description=readmeText,
     author='Jan Štětina',
     author_email='zaantar@gmail.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
+    url='https://github.com/zaantar/gcal-mm-status',
+    license=licenseText,
     packages=find_packages(exclude=('tests', 'docs'))
 )
