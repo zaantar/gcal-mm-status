2d9db1af5142b7e027b9b07d71a620ac2fe20a88
jan <jan.s@icanlocalize.com>
2020-01-18 19:21:45 +0100

    Require Python 3.6 or above.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 import one_shot
 import log
+import sys
+
+if not sys.version_info.major == 3 or (sys.version_info.major == 3 and sys.version_info.minor < 6):
+    print('This software requires Python 3 from the version 3.6 or above.')
 
 log.set_log_level(log.Level.DEBUG)
 one_shot.run()
