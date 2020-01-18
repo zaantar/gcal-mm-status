42548a5a85e1fe22b288397ad1f438d16e82caee
jan <jan.s@icanlocalize.com>
2020-01-18 19:21:32 +0100

    Rudimentary system daemon implementation - proof of concept.

---
+++ src/daemon_starter.py
@@ -0,0 +1,32 @@
+import daemon
+from log import log, Level as LogLevel, set_log_level
+import signal
+import lockfile
+import sys
+
+
+# Needs to be executed with root permissions:
+# sudo python3 src/daemon_starter.py
+
+def shutdown(signum, frame):
+    log('Shutting down with signal %d' % signum.value)
+    sys.exit(0)
+
+
+def init(preserve_output=False):
+    with daemon.DaemonContext(
+            stdout=sys.stdout if preserve_output else None,
+            stderr=sys.stderr if preserve_output else None,
+            stdin=sys.stdin if preserve_output else None,
+            signal_map={
+                signal.SIGTERM: shutdown,
+                signal.SIGTSTP: shutdown,
+            },
+            pidfile=lockfile.FileLock('/var/run/gcal-mm-status.pid')
+    ):
+        set_log_level(LogLevel.DEBUG)
+        log('Hello world!')
+        shutdown(signal.SIGTERM, None)
+
+
+init(preserve_output=True)
